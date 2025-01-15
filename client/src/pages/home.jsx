0bb669422cade5a7fde5d7678903ffe664477bff
Fuadmikayil <mikayilov2006@gmail.com>
2025-01-16 02:43:39 +0400

    cms

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -25,7 +25,7 @@ const HomePage = () => {
       <Hero
         title="Fresh Arrivals Online"
         subtitle="Discover Our Newest Collection Today."
-        btnText="View Collection"
+        btnText="View Collaection"
         btnHref=""
         img={heroImgOnline}
       />
